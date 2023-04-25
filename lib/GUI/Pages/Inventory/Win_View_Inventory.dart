3b2762c78d1f793787b87cf8df5e1d3938e4a511
jonas <jonasaustinland@tamu.edu>
2023-04-25 00:30:47 -0500

    View inventory translates

--- lib/GUI/Pages/Inventory/Win_View_Inventory.dart
+++ lib/GUI/Pages/Inventory/Win_View_Inventory.dart
@@ -1,10 +1,8 @@
 import 'package:csce315_project3_13/GUI/Components/Login_Button.dart';
-import 'package:csce315_project3_13/GUI/Pages/Management/Win_Add_Smoothie.dart';
+import 'package:csce315_project3_13/Inherited_Widgets/Translate_Manager.dart';
 import 'package:csce315_project3_13/Manager_View/Win_Manager_View.dart';
-import 'package:csce315_project3_13/Services/general_helper.dart';
+import 'package:csce315_project3_13/Services/google_translate_API.dart';
 import 'package:csce315_project3_13/Services/inventory_helper.dart';
-import 'package:csce315_project3_13/Services/menu_item_helper.dart';
-import 'package:csce315_project3_13/Services/view_helper.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../../../Inherited_Widgets/Color_Manager.dart';
 import '../../../Models/models_library.dart';
@@ -21,16 +19,59 @@ class Win_View_Inventory extends StatefulWidget {
 
 
 class _Win_View_Inventory_State extends State<Win_View_Inventory> {
+
+  //Keeps track of whether to update name or not
+  bool call_set_translation = true;
+
+  google_translate_API _google_translate_api = google_translate_API();
+
+  //Strings for display
+  List<String> list_page_texts_originals = ["Inventory Item Management", "Edit Item Amount", "New Amount",  "CANCEL", "CONFIRM",  "Not enough inventory to satisfy the requested change.", "OK", "Unable to change amount for this item.", "Successfully Removed Item", "Unable to remove inventory item", "Confirm Item Deletion", "Are you sure you want to delete", "Stock", "Remove from Inventory",  "Edit Amount", "Successfully Added Item",  "Unable to add item", "New Inventory Item Creation",  "Ingredient Name", "Amount in Stock", "Unit", "Date Ordered", "Expiration Date", "Conversion", "ADD ITEM", "available", "Return to Manager View", "Add Inventory" ];
+  List<String> list_page_texts = ["Inventory Item Management", "Edit Item Amount", "New Amount",  "CANCEL", "CONFIRM",  "Not enough inventory to satisfy the requested change.", "OK", "Unable to change amount for this item.", "Successfully Removed Item", "Unable to remove inventory item", "Confirm Item Deletion", "Are you sure you want to delete", "Stock", "Remove from Inventory",  "Edit Amount", "Successfully Added Item",  "Unable to add item", "New Inventory Item Creation",  "Ingredient Name", "Amount in Stock", "Unit", "Date Ordered", "Expiration Date", "Conversion", "ADD ITEM", "available", "Return to Manager View", "Add Inventory" ];
+  String text_page_header = "Inventory Item Management";
+  String text_item_amount = "Edit Item Amount";
+  String text_hint_new_amount = "New Amount";
+  String text_cancel_button = "CANCEL";
+  String text_confirm_button = "CONFIRM";
+  String text_not_enough_inventory = "Not enough inventory to satisfy the requested change.";
+  String text_ok_button = "OK";
+  String text_unable_to_change_amount = "Unable to change amount for this item.";
+  String text_message_text_rem = "Successfully Removed Item";
+  String text_message_text_rem_alt = "Unable to remove inventory item";
+  String text_confirm_item_deletion = "Confirm Item Deletion";
+  String text_certain_you_want_del = "Are you sure you want to delete";
+  String text_stock = "Stock";
+  String text_remove_from_inventory = "Remove from Inventory";
+  String text_edit_amount = "Edit Amount";
+  String text_message_text_add = "Successfully Added Item";
+  String text_message_text_add_alt = "Unable to add item";
+  String text_new_item_creation = "New Inventory Item Creation";
+  String text_ingredient_name = "Ingredient Name";
+  String text_amount_in_stock = "Amount in Stock";
+  String text_amount_ordered = "Amount Ordered";
+  String text_unit = "Unit";
+  String text_date_ordered = "Date Ordered";
+  String text_expiration_date = "Expiration Date";
+  String text_conversion = "Conversion";
+  String text_add_item = "ADD ITEM";
+  String text_available = "available";
+  String text_ret_man = "Return to Manager View";
+  String text_add_inventory = "Add Inventory";
+
+
+
+
+
+
   int visibility_ctrl = 0;
-  String title = 'Manage Smoothies';
   bool _isLoading = true;
   Map<String, num> inventoryItems = {};
   inventory_item_helper inv_helper = inventory_item_helper();
 
   void getData() async {
-    print('Building Page...');
+    print("Building Page...");
     inventoryItems = await inv_helper.get_inventory_items();
-    print('Obtained Inventory...');
+    print("Obtained Inventory...");
     setState(() {
       _isLoading = false;
     });
@@ -43,21 +84,21 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
         TextEditingController amountController =
         TextEditingController(text: currentAmount.toString());
         return AlertDialog(
-          title: const Text('Edit Item Amount'),
+          title: Text(text_item_amount),
           content: TextFormField(
             controller: amountController,
-            decoration: const InputDecoration(hintText: 'New Amount...'),
+            decoration: InputDecoration(hintText:  text_hint_new_amount + "..."),
             keyboardType: TextInputType.number,
           ),
           actions: <Widget>[
             TextButton(
-              child: const Text('CANCEL'),
+              child: Text(text_cancel_button),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: const Text('CONFIRM'),
+              child: Text(text_confirm_button),
               onPressed: () async {
                 Navigator.of(context).pop();
                 try {
@@ -70,14 +111,14 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                         builder: (BuildContext context) {
                           return AlertDialog(
                             title: const Icon(Icons.error_outline_sharp),
-                            content: const Text(
-                                'Not enough inventory to satisfy the requested change.'),
+                            content: Text(
+                                text_not_enough_inventory),
                             actions: [
                               TextButton(
                                   onPressed: () {
                                     Navigator.of(context).pop();
                                   },
-                                  child: const Text('OK'))
+                                  child: Text(text_ok_button))
                             ],
                           );
                         });
@@ -92,14 +133,14 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                       builder: (BuildContext context) {
                         return AlertDialog(
                           title: const Icon(Icons.error_outline_sharp),
-                          content: const Text(
-                              'Unable to change amount for this item.'),
+                          content: Text(
+                              text_unable_to_change_amount),
                           actions: [
                             TextButton(
                                 onPressed: () {
                                   Navigator.of(context).pop();
                                 },
-                                child: const Text('OK'))
+                                child: Text(text_ok_button))
                           ],
                         );
                       });
@@ -115,23 +156,23 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
   // INSERT INTO menu_items (menu_item_id, menu_item, item_price, amount_in_stock, type, status) VALUES(407, 'The Smoothie Squad Special small', 6.18, 60, 'smoothie', 'available')
   void confirmInventoryItemRemoval(String itemName) {
     Icon message_icon = const Icon(Icons.check);
-    String message_text = 'Successfully Removed Item';
+    String message_text = text_message_text_rem;
     showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Confirm Item Deletion'),
-          content: Text('Are you sure you want to delete $itemName ?'),
+          title: Text(text_confirm_item_deletion),
+          content: Text( text_certain_you_want_del + " $itemName ?"),
           actions: <Widget>[
             TextButton(
-              child: const Text('CANCEL'),
+              child: Text(text_cancel_button),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: const Text('CONFIRM'),
+              child: Text(text_confirm_button),
               onPressed: () async {
                 try {
                   await inv_helper.deleteInventoryItem(itemName);
@@ -143,7 +184,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 } catch (exception) {
                   print(exception);
                   message_icon = const Icon(Icons.error_outline_outlined);
-                  message_text = 'Unable to remove inventory item';
+                  message_text = text_message_text_rem_alt;
                 } finally {
                   showDialog(
                     barrierDismissible: false,
@@ -157,7 +198,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                             onPressed: () {
                               Navigator.of(context).pop();
                             },
-                            child: const Text('OK'),
+                            child: Text(text_ok_button),
                           )
                         ],
                       );
@@ -215,7 +256,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
               ),
             ),
             subtitle: Text(
-              'Stock: ${entry.value}',
+              text_stock + ": ${entry.value}",
               style: TextStyle(
                 fontSize: 20,
                 color: _text_color.withAlpha(122),
@@ -228,7 +269,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   IconButton(
-                    tooltip: 'Remove from Inventory',
+                    tooltip: text_remove_from_inventory,
                     icon: const Icon(Icons.delete),
                     color: _text_color.withAlpha(122),
                     onPressed: () {
@@ -237,7 +278,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                     iconSize: 35,
                   ),
                   IconButton(
-                    tooltip: 'Edit Amount',
+                    tooltip: text_edit_amount,
                     icon: const Icon(Icons.add),
                     color: _text_color.withAlpha(122),
                     onPressed: () {
@@ -266,14 +307,14 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
     TextEditingController _conversion = TextEditingController();
 
     Icon message_icon = const Icon(Icons.check);
-    String message_text = 'Successfully Added Item';
+    String message_text = text_message_text_add;
 
     showDialog(
       barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('New Inventory Item Creation'),
+          title: Text(text_new_item_creation),
           content: SizedBox(
             width: 400,
             height: 400,
@@ -283,7 +324,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _ingredient_name,
                   decoration:  InputDecoration(
-                    hintText: 'Ingredient Name...',
+                    hintText: text_ingredient_name + "...",
                     filled: true,
                     fillColor: Colors.white,
                     border: OutlineInputBorder(
@@ -294,7 +335,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _amount_inv_stock,
                     decoration:  InputDecoration(
-                      hintText: 'Amount in Stock...',
+                      hintText: text_amount_in_stock + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -305,7 +346,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _amount_ordered,
                     decoration:  InputDecoration(
-                      hintText: 'Amount Ordered...',
+                      hintText: text_amount_ordered + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -316,7 +357,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _unit,
                     decoration:  InputDecoration(
-                      hintText: 'Unit...',
+                      hintText: text_unit + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -327,7 +368,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _date_ordered,
                     decoration:  InputDecoration(
-                      hintText: 'Date Ordered...',
+                      hintText: text_date_ordered + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -338,7 +379,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _expiration_date,
                     decoration:  InputDecoration(
-                      hintText: 'Expiration Date...',
+                      hintText:  text_expiration_date + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -349,7 +390,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 TextFormField(
                   controller: _conversion,
                     decoration:  InputDecoration(
-                      hintText: 'Conversion...',
+                      hintText: text_conversion + "...",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
@@ -362,18 +403,18 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
           ),
           actions: <Widget>[
             TextButton(
-              child: const Text('CANCEL'),
+              child: Text(text_cancel_button),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: const Text('ADD ITEM'),
+              child: Text(text_add_item),
               onPressed: () async {
                 try {
                   inventory_item_obj new_item = inventory_item_obj(
                       0,
-                      'available',
+                      text_available,
                       _ingredient_name.text,
                       int.parse(_amount_inv_stock.text),
                       int.parse(_amount_ordered.text),
@@ -393,7 +434,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 {
                   print(exception);
                   message_icon = const Icon(Icons.error_outline_outlined);
-                  message_text = 'Unable to add item';
+                  message_text = text_message_text_add_alt;
                 }
                 finally{
                   showDialog(
@@ -407,7 +448,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                                 onPressed: (){
                                   Navigator.of(context).pop();
                                 },
-                                child: const Text('OK'))
+                                child: Text(text_ok_button))
                           ],
                         );
                       });
@@ -450,13 +491,74 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
   @override
   Widget build(BuildContext context){
     final _color_manager = Color_Manager.of(context);
+
+
+
+
+    // ToDo Implement the below translation functionality
+    final _translate_manager = Translate_Manager.of(context);
+
+    Future<void> set_translation() async {
+      call_set_translation = false;
+
+      //set the new Strings here
+      list_page_texts = (await _google_translate_api.translate_batch(list_page_texts_originals,_translate_manager.chosen_language));
+      text_page_header = list_page_texts[0];
+      text_item_amount = list_page_texts[1];
+      text_hint_new_amount = list_page_texts[2];
+      text_cancel_button = list_page_texts[3];
+      text_confirm_button = list_page_texts[4];
+      text_not_enough_inventory = list_page_texts[5];
+      text_ok_button = list_page_texts[6];
+      text_unable_to_change_amount = list_page_texts[7];
+      text_message_text_rem = list_page_texts[8];
+      text_message_text_rem_alt = list_page_texts[9];
+      text_confirm_item_deletion = list_page_texts[10];
+      text_certain_you_want_del = list_page_texts[11];
+      text_stock = list_page_texts[12];
+      text_remove_from_inventory = list_page_texts[13];
+      text_edit_amount = list_page_texts[14];
+      text_message_text_add = list_page_texts[15];
+      text_message_text_add_alt = list_page_texts[16];
+      text_new_item_creation = list_page_texts[17];
+      text_ingredient_name = list_page_texts[18];
+      text_amount_in_stock = list_page_texts[19];
+      text_amount_ordered = list_page_texts[20];
+      text_unit = list_page_texts[21];
+      text_date_ordered = list_page_texts[22];
+      text_expiration_date = list_page_texts[23];
+      text_conversion = list_page_texts[24];
+      text_add_item = list_page_texts[25];
+      text_available = list_page_texts[26];
+      text_ret_man = list_page_texts[27];
+      // text_add_inventory = list_page_texts[28];
+
+
+
+
+      setState(() {
+      });
+    }
+
+    if(call_set_translation){
+      set_translation();
+    }else{
+      call_set_translation = true;
+    }
+
+    //Translation functionality end
+
+
+
+
+
     return Scaffold(
       appBar: Page_Header(
         context: context,
-        pageName: "Inventory Item Management",
+        pageName: text_page_header,
         buttons: [
           IconButton(
-            tooltip: "Return to Manager View",
+            tooltip: text_ret_man,
             padding: const EdgeInsets.only(left: 25, right: 10),
             onPressed: ()
             {
@@ -494,7 +596,7 @@ class _Win_View_Inventory_State extends State<Win_View_Inventory> {
                 newItemSubWin();
               },
               buttonWidth: 200,
-              buttonName: 'Add Inventory',
+              buttonName: text_add_inventory,
             ),
             // Login_Button(
             //   onTap: () {
