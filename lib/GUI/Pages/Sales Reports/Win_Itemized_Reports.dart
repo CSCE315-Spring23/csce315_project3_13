17d2e81457a5cf191fec4030d39c814c5901ca37
swkeylon <swkeylon@gmail.com>
2023-04-29 18:47:02 -0500

    Added basic itemized reports as a template of the z reports table. Need more info on how things load in flutter to finish functionality.

---
+++ lib/GUI/Pages/Sales Reports/Win_Itemized_Reports.dart
@@ -0,0 +1,154 @@
+import 'package:csce315_project3_13/GUI/Components/Login_Button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
+
+import '../../../Inherited_Widgets/Color_Manager.dart';
+import '../../../Models/models_library.dart';
+import '../../../Services/general_helper.dart';
+import '../../../Services/reports_helper.dart';
+import '../../Components/Page_Header.dart';
+import 'Win_Reports_Hub.dart';
+
+class Win_Itemized_Reports extends StatefulWidget {
+  static const String route = '/z-reports';
+  const Win_Itemized_Reports({super.key});
+
+  @override
+  State<Win_Itemized_Reports> createState() => _Win_Itemized_ReportsState();
+}
+
+class _Win_Itemized_ReportsState extends State<Win_Itemized_Reports> {
+  general_helper gen_help = general_helper();
+  reports_helper rep_help = reports_helper();
+  DateTime date = DateTime.now();
+  List<sales_report_row> rows = [];
+  bool dataLoaded = true;
+
+  void getData(String date1, String date2) async
+  {
+    print("Getting data...");
+
+    rows = rep_help.generate_sales_report(date1, date2);
+    setState(()
+    {
+      dataLoaded = true;
+    });
+  }
+
+  Widget salesList(List<sales_report_row> rows, Color _text_color, Color _box_color)
+  {
+    print("Constructing sales list...");
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: rows.length,
+      itemBuilder: (BuildContext context, int index) =>
+          Card(
+              color: _box_color,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.outline,
+                ),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('${rows[index].item_name} sold \$${rows[index].amount_sold} units and made \$${rows[index].total_revenue} in revenue.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: _text_color,
+                  ),
+                ),
+              )
+          ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _color_manager = Color_Manager.of(context);
+    return Scaffold(
+      backgroundColor: _color_manager.background_color,
+      appBar: Page_Header(
+        context: context,
+        pageName: "Itemized Reports",
+        buttons: [
+          IconButton(
+            tooltip: "Return to Reports Hub",
+            padding: const EdgeInsets.only(left: 25, right: 10),
+            onPressed: ()
+            {
+              Navigator.pushReplacementNamed(context,Win_Reports_Hub.route);
+
+            },
+            icon: const Icon(Icons.close_rounded),
+            iconSize: 40,
+          ),],
+      ),
+      body: !dataLoaded ?  Center(
+        child: SpinKitRing(color: _color_manager.primary_color) ,
+      ) : Center(
+        child: SizedBox(
+          child: salesList(rows, _color_manager.text_color, _color_manager.active_color),
+        ),
+      ),
+      bottomSheet: Container
+        (
+        height: 75,
+        color: _color_manager.primary_color,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Login_Button(
+              onTap: () async {
+                DateTime ? newDate = await showDatePicker(
+                  context: context,
+                  initialDate: date,
+                  firstDate: DateTime(2020),
+                  lastDate: DateTime(2025),
+                );
+
+                if (newDate == null) return;
+
+                setState(() => date = newDate);
+
+                var formatter = DateFormat('MM/dd/yyyy');
+                String formattedDate = formatter.format(date);
+
+                double z_rep = await rep_help.get_z_report(formattedDate);
+
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Text("Z report for the chosen date: $z_rep"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('OK'),
+                        )
+                      ],
+                    );
+                  },
+                );
+              },
+              buttonName: "Get Z Report", fontSize: 18, buttonWidth: 180,),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
